7872663a9e2a70e3843b73148d5d166100a48102
jehad630 <devjehad630@gmail.com>
2025-11-21 18:28:06 +0300

    adding widgets layouts for every platform and start creating app styles page

--- lib/features/views/dash_board_view.dart
+++ lib/features/views/dash_board_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashborad/features/views/adaptive_layout.dart';
+import 'package:responsive_dashborad/features/widgets/dashboard_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
   }
 }
